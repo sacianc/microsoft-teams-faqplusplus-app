29c2e7298212bae6bcbdb6dbd998df9eb8583c31
Santiago Cianciulli <sacianc@microsoft.com>
2020-03-11 13:39:01 -0300

    fix v13

--- Source/Microsoft.Teams.Apps.FAQPlusPlus/Cards/ResponseCard.cs
+++ Source/Microsoft.Teams.Apps.FAQPlusPlus/Cards/ResponseCard.cs
@@ -79,7 +79,7 @@ namespace Microsoft.Teams.Apps.FAQPlusPlus.Cards
                         Data = new AdaptiveOpenUrlAction
                         {
                             Title = "Videoteca",
-                            Url = new uri("https://www.stream.com"),
+                            Url = new Uri("https://www.stream.com"),
                         }
                     }
                 }
